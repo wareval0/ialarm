60fff6f4b7e4689a2a9ae2fb1e761e1188b0a40c
Juan Pablo Baldion <j.baldion@uniandes.edu.co>
2025-03-14 15:00:25 -0500

    Version previa Pantalla crear tarea

--- ialarm_mobile/lib/main.dart
+++ ialarm_mobile/lib/main.dart
@@ -12,7 +12,8 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Formulario'),
+          title: const Text('Crear Tarea', style: TextStyle(color: Colors.white)),
+          backgroundColor: const Color(0xFF083D77),
         ),
         body: const Formulario(),
       ),
@@ -40,16 +41,20 @@ class _FormularioState extends State<Formulario> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(36.0),
       child: Form(
         key: _formKey,
+
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const Text('Título'),
+            const Text('Título',
+            style: TextStyle(fontSize: 24.0, color: Color(0xFF2E4057))),
             TextFormField(
               decoration: const InputDecoration(
+                hintText: 'Título de la tarea',
                 border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 12.0),
               ),
               validator: (value) {
                 if (value == null || value.isEmpty) {
@@ -58,12 +63,16 @@ class _FormularioState extends State<Formulario> {
                 return null;
               },
             ),
-            const SizedBox(height: 16.0),
-            const Text('Descripción'),
+            const SizedBox(height: 32.0),
+            const Text('Descripción',
+            style: TextStyle(fontSize: 24.0, color: Color(0xFF2E4057))),
             TextFormField(
               decoration: const InputDecoration(
+                hintText: 'Escribe una descripción lo más detallada que quieras',
                 border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 12.0),
               ),
+              maxLines: 5,
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return 'Por favor ingrese una descripción';
@@ -72,32 +81,43 @@ class _FormularioState extends State<Formulario> {
               },
             ),
             const SizedBox(height: 16.0),
-            const Text('Materia'),
-            DropdownButtonFormField<String>(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-              ),
-              items: <String>['DSW', 'Dalgo', 'UX', 'Sistrans']
-                  .map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (newValue) {
-                setState(() {
-                  _materiaSeleccionada = newValue;
-                });
-              },
-              validator: (value) {
-                if (value == null) {
-                  return 'Por favor seleccione una materia';
-                }
-                return null;
-              },
+            Row(
+              children: [
+                const Text('Materia ',
+                style: TextStyle(fontSize: 24.0, color: Color(0xFF2E4057))),
+                const SizedBox(width: 36.0),
+                Expanded(
+                  child: DropdownButtonFormField<String>(
+                    decoration: const InputDecoration(
+                      hintText: 'Seleccionar',
+                      border: OutlineInputBorder(),
+                      contentPadding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 12.0),
+                    ),
+                    items: <String>['DSW', 'Dalgo', 'UX', 'Sistrans']
+                        .map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (newValue) {
+                      setState(() {
+                        _materiaSeleccionada = newValue;
+                      });
+                    },
+                    validator: (value) {
+                      if (value == null) {
+                        return 'Por favor seleccione una materia';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+              ],
             ),
             const SizedBox(height: 16.0),
-            const Text('Opciones'),
+            const Text('Tipo de Alarma',
+            style: TextStyle(fontSize: 24.0, color: Color(0xFF2E4057))),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -111,12 +131,18 @@ class _FormularioState extends State<Formulario> {
                         });
                       },
                       style: OutlinedButton.styleFrom(
+                        backgroundColor: _opcionSeleccionada == 'Sonar' 
+                            ? const Color(0xFF083D77) // Azul seleccionado
+                            : Colors.transparent, // Fondo transparente cuando no está seleccionado
+                        foregroundColor: _opcionSeleccionada == 'Sonar' 
+                            ? Colors.white // Texto blanco cuando está seleccionado
+                            : const Color(0xFF2E4057), // Texto normal cuando no está seleccionado
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(18.0),
                         ),
                         side: BorderSide(
                           color: _opcionSeleccionada == 'Sonar'
-                              ? Colors.blue
+                              ? const Color(0xFF083D77) // Borde azul cuando está seleccionado
                               : Colors.grey,
                         ),
                       ),
@@ -134,12 +160,18 @@ class _FormularioState extends State<Formulario> {
                         });
                       },
                       style: OutlinedButton.styleFrom(
+                        backgroundColor: _opcionSeleccionada == 'Vibrar' 
+                            ? const Color(0xFF083D77) 
+                            : Colors.transparent,
+                        foregroundColor: _opcionSeleccionada == 'Vibrar' 
+                            ? Colors.white 
+                            : const Color(0xFF2E4057),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(18.0),
                         ),
                         side: BorderSide(
                           color: _opcionSeleccionada == 'Vibrar'
-                              ? Colors.blue
+                              ? const Color(0xFF083D77)
                               : Colors.grey,
                         ),
                       ),
@@ -157,12 +189,18 @@ class _FormularioState extends State<Formulario> {
                         });
                       },
                       style: OutlinedButton.styleFrom(
+                        backgroundColor: _opcionSeleccionada == 'Notificar' 
+                            ? const Color(0xFF083D77) 
+                            : Colors.transparent,
+                        foregroundColor: _opcionSeleccionada == 'Notificar' 
+                            ? Colors.white 
+                            : const Color(0xFF2E4057),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(18.0),
                         ),
                         side: BorderSide(
                           color: _opcionSeleccionada == 'Notificar'
-                              ? Colors.blue
+                              ? const Color(0xFF083D77)
                               : Colors.grey,
                         ),
                       ),
@@ -172,14 +210,17 @@ class _FormularioState extends State<Formulario> {
                 ),
               ],
             ),
+
             const SizedBox(height: 16.0),
-            const Text('Fecha'),
+            const Text('Fecha Límite',
+            style: TextStyle(fontSize: 24.0, color: Color(0xFF2E4057))),
             Row(
               children: [
                 Expanded(
                   child: TextFormField(
                     controller: _diaController,
                     decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 12.0),
                       border: OutlineInputBorder(),
                       hintText: 'DD',
                     ),
@@ -197,6 +238,7 @@ class _FormularioState extends State<Formulario> {
                   child: TextFormField(
                     controller: _mesController,
                     decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 12.0),
                       border: OutlineInputBorder(),
                       hintText: 'MM',
                     ),
@@ -214,6 +256,7 @@ class _FormularioState extends State<Formulario> {
                   child: TextFormField(
                     controller: _anoController,
                     decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 12.0),
                       border: OutlineInputBorder(),
                       hintText: 'AAAA',
                     ),
@@ -227,7 +270,7 @@ class _FormularioState extends State<Formulario> {
                   ),
                 ),
                 IconButton(
-                  icon: const Icon(Icons.calendar_today),
+                  icon: const Icon(Icons.calendar_today_rounded),
                   onPressed: () async {
                     DateTime? picked = await showDatePicker(
                       context: context,
@@ -248,30 +291,43 @@ class _FormularioState extends State<Formulario> {
               ],
             ),
             const SizedBox(height: 16.0),
-            const Text('Grupo'),
-            DropdownButtonFormField<String>(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-              ),
-              items: <String>['Grupo A', 'Grupo B', 'Grupo C']
-                  .map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (newValue) {
-                setState(() {
-                  _grupoSeleccionado = newValue;
-                });
-              },
-              validator: (value) {
-                if (value == null) {
-                  return 'Por favor seleccione un grupo';
-                }
-                return null;
-              },
+            Row(
+              children: [
+                const Text(
+                  'Grupo ',
+                  style: TextStyle(fontSize: 24.0, color: Color(0xFF2E4057)),
+                  
+                ),
+                const SizedBox(width: 36.0), // Agrega espacio entre el label y el campo
+                Expanded(
+                  child: DropdownButtonFormField<String>(
+                    decoration: const InputDecoration(
+                      hintText: 'Seleccionar',
+                      border: OutlineInputBorder(),
+                      contentPadding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 12.0),
+                    ),
+                    items: <String>['Grupo A', 'Grupo B', 'Grupo C'].map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (newValue) {
+                      setState(() {
+                        _grupoSeleccionado = newValue;
+                      });
+                    },
+                    validator: (value) {
+                      if (value == null) {
+                        return 'Por favor seleccione un grupo';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+              ],
             ),
+
             const SizedBox(height: 16.0),
             Align(
               alignment: Alignment.bottomRight,
